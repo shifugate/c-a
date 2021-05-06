17bcab8d7c4477286f293746564808f3f4a0950e
shifugate <shifugate@gmail.com>
2021-05-06 11:52:30 +0100

    Add files via upload

---
+++ dist/aframe-transparent-video-shader.umd.js
@@ -0,0 +1,2 @@
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t():"function"==typeof define&&define.amd?define(t):t()}(0,function(){AFRAME.registerShader("transparent-video",{schema:{src:{type:"map"}},init:function(e){var t=new THREE.VideoTexture(e.src);t.minFilter=THREE.LinearFilter,t.format=THREE.RGBAFormat,this.material=new THREE.MeshLambertMaterial({map:t})}})});
+//# sourceMappingURL=aframe-transparent-video-shader.umd.js.map
